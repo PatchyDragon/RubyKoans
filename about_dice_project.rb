d505e4887d84b2fb9399fb3a7d17eb33b3b17148
Aztectornado <aztectornado@gmail.com>
2016-03-22 11:51:22 -0600

    enlighten about_dice_project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  def roll(i) #Why is this considered zero arguments? Whyyyyy?
+    chaos = Random.new
+    @val = []
+    i.times { values << chaos.rand(5) + 1 }
+  end
+
+  def values
+    @val
+  end
+
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
